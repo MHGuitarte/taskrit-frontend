8dd8911f92d9cbe8fd4f4fe82aa717d473c44072
MHGuitarte <guimhur@gmail.com>
2021-05-29 18:28:18 +0200

    feat(): add vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
